abe4e7b21dbd8e0956e5b6d73492300c32ebf16d
PIPICBA <lichimedina05@gmail.com>
2025-06-21 17:46:18 -0300

    Acá están los videos y un par de correcciones

--- tarea.py
+++ tarea.py
@@ -1,10 +1,15 @@
 import math
 
-def calcular_hipotenusa():
-    a = float(input("Ingrese el primer ctaeto: "))
-    b = float(input("Ingrese el segundo cateto: "))
-    hipotenusa = math.sqrt(a^2 + b^2)
+def calcular_hipotenusa(a,b): 
+    hipotenusa = math.sqrt(a**2 + b**2)
     print(f"La hipotenusa es: {hipotenusa}")
 
+def calcular_area_triangulo(base, altura):
+    area = (base * altura) / 2
+    print(f"El área del triángulo es: {area}")
 
-calcular_hipotenusa()
+a = float(input("Ingrese el primer cateto: "))
+b = float(input("Ingrese el segundo cateto: "))
+calcular_hipotenusa(a,b)
+calcular_area_triangulo(a, b)
+
